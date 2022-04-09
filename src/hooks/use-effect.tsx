cc5503454c9201373e18d0b5a2d571d66d8ba4a5
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-04-09 12:33:50 -0400

    use effect example ok and working with useReducer

--- src/hooks/use-effect.tsx
+++ src/hooks/use-effect.tsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import { FC, useEffect, useState } from 'react'
 
-export const UseEffect = () => {
-  return <div>Hello am UseEffect!</div>
+export const UseEffect: FC = () => {
+  const [count, setCount] = useState(1)
+  const [countTwo, setCountTwo] = useState(2)
+  const [, setKeyPressed] = useState('')
+
+  useEffect(() => {
+    document.title = (count + countTwo).toString()
+    console.log(document.title)
+  }, [count, countTwo])
+
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      setKeyPressed(e.code)
+    }
+    window.addEventListener('keydown', handleKeyPress)
+
+    return () => window.removeEventListener('keydown', handleKeyPress)
+  }, [])
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Next</button>
+      <button onClick={() => setCountTwo(countTwo + 1)}>Next</button>
+    </div>
+  )
 }
