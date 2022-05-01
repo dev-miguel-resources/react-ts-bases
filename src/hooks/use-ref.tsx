7272c108ffeb2c8fe97c5406816f2d950e691007
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-05-01 12:26:58 -0400

    useRef component ok

--- src/hooks/use-ref.tsx
+++ src/hooks/use-ref.tsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import { FC, useRef, useEffect } from 'react'
 
-export const UseRef = () => {
-  return <div>Hello am UseRef!</div>
+export const UseRef: FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    const shouldFocus = Math.random() > 0.5
+    console.log(shouldFocus)
+    if (shouldFocus) {
+      inputRef.current?.focus()
+    }
+  }, [])
+
+  return (
+    <>
+      <h1>Use Ref</h1>
+      <input ref={inputRef} />
+    </>
+  )
 }
